16773eb21d2ab8aad592afd23c1cea2c6a7e0ee2
sahayajessemonisha <41037961+sahayajessemonisha@users.noreply.github.com>
2018-09-12 14:16:50 +0530

    Create sum of k integer find.py

---
+++ beginner/sum of k integer find.py
@@ -0,0 +1,11 @@
+a=[]
+N=int(input("enter size:"))
+for x in range(0,N):
+   y=int(input("enter value:"))
+   a.append(y);
+   print(a);
+s=0;
+K=int(input("enter a number:"))
+for K in range(0,K):
+    S=S+a[K]
+print(s);
