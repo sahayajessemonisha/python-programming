a8912bf0574d23ad9c16dd9e20dfee3313654a5d
sahayajessemonisha <41037961+sahayajessemonisha@users.noreply.github.com>
2018-09-13 15:35:37 +0530

    Create Check value is palindrome or not.py

---
+++ beginner/Check value is palindrome or not.py
@@ -0,0 +1,11 @@
+n=int(input("enter a value:")
+x=n
+rev=0
+while(n>0):
+   c=n%10
+   rev=rev*10+c
+if(x==Rev):
+  print("the number is palindrome") 
+else:
+  print("the number is't palindrome") 
+
