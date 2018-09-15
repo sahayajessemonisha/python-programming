e05fbb6345518c775be201d09c0e3706a09f3b4c
sahayajessemonisha <41037961+sahayajessemonisha@users.noreply.github.com>
2018-09-15 22:03:43 +0530

    Create Check Armstrong number.py

---
+++ beginner/Check Armstrong number.py
@@ -0,0 +1,14 @@
+n=371
+if(n<=100000):
+  temp=n
+  sum=0
+  while(n>0):
+    r=n%10
+    sum=sum+(r*r*r) 
+    n=n//10
+  if(temp==sum):
+         print('yes') 
+  else:
+         print('no') 
+else:
+    print('invalid') 
