4946f821472609db008ebfa3d350f7337f8ada44
sahayajessemonisha <41037961+sahayajessemonisha@users.noreply.github.com>
2018-12-29 15:02:09 +0530

    Create swap program.py

---
+++ beginner/swap program.py
@@ -0,0 +1,7 @@
+x=int(input("enter a number:"))
+y=int(input("enter a number:"))
+s=x
+x=y
+y=s
+print('the values of x after swapping:{}'.format(x))
+print('the values of y after swapping:{}'.format(y))
